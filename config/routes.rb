2c05d38555661d65c816b9bc1c86fe5cc5308259
AgneloFH <tech.agnelo@gmail.com>
2024-11-16 18:19:32 -0300

    refactoring Application settings.

--- config/routes.rb
+++ config/routes.rb
@@ -6,28 +6,25 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-    namespace :admin do
-      get 'dashboard', to: 'dashboards#show', as: :dashboard
-      get 'discover', to: 'discovers#index', as: :discover
-      get 'chat', to: 'chats#index', as: :chat
-      get 'listing', to: 'listings#index', as: :listing
-      get 'customer', to: 'customers#index', as: :customer
-      get 'analytics', to: 'analytics#index', as: :analytics
-      get 'transaction', to: 'transactions#index', as: :transaction
-      namespace :real_estate do
-        namespace :settings do
+  namespace :admin do
+    get 'dashboard', to: 'dashboards#show', as: :dashboard
+    get 'discover', to: 'discovers#index', as: :discover
+    get 'chat', to: 'chats#index', as: :chat
+    get 'listing', to: 'listings#index', as: :listing
+    get 'customer', to: 'customers#index', as: :customer
+    get 'analytics', to: 'analytics#index', as: :analytics
+    get 'transaction', to: 'transactions#index', as: :transaction
+    get 'settings', to: 'settings#index', as: :settings
+      #namespace :real_estate do
+      #  namespace :settings do
           # A rota para index de settings
-          resources :settings, only: :index
+      #    resources :settings, only: :index
 
-          namespace :settings do
-            resources :property_types, path: 'property_types', only: [:index, :new, :create, :edit, :update, :destroy, :show]
-          end
-        end
-      end
+       #   namespace :settings do
+           # resources :property_types, path: 'property_types', only: [:index, :new, :create, :edit, :update, :destroy, :show]
+        #  end
+       # end
+      #end
     end
-
-
-
-
   get "up" => "rails/health#show", as: :rails_health_check
 end
